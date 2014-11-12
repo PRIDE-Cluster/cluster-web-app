5d4e26dcc4d854997f8bdee6b8c4e2341e0008f9
Jose A Dianes <jadianes@gmail.com>
2014-11-12 13:20:06 +0000

    dev config refactored out

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 module.exports = function(config){
     config.set({
 
-        basePath : './',
+        basePath : './src/main/webapp/app/',
 
         files : [
             'bower_components/angular/angular.js',
