5ccc8218bb4a5115dbaed90d78f5775a0a7b5c28
Jose A Dianes <jadianes@gmail.com>
2014-11-18 15:17:05 +0000

    added missing dep in karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function(config){
             'bower_components/angular-animate/angular-animate.js',
             'bower_components/angular-resource/angular-resource.js',
             'bower_components/angular-mocks/angular-mocks.js',
+            'bower_components/angularjs-nvd3-directives/dist/angularjs-nvd3-directives.js',
             'app.js',
             'components/**/*.js',
             'view*/**/*.js'
