60881faf63277b627750a030694ddc9e5d870fde
Jose A Dianes <jadianes@gmail.com>
2014-12-03 16:29:14 +0000

    clean up of clusterListDirective

--- src/main/webapp/components/directives/clusterListFilters-directive/clusterListFilters-directive.js
+++ src/main/webapp/components/directives/clusterListFilters-directive/clusterListFilters-directive.js
@@ -11,44 +11,33 @@ clusterListFiltersDirective.directive('prcClusterListFilters', function() {
     return {
         restrict: 'E',
         scope: {
-            sequence : '=sequence'
+
         },
         controller: "ClusterListFiltersCtrl",
         templateUrl: 'components/directives/clusterListFilters-directive/clusterListFilters-directive.html'
     };
 });
 
-clusterListFiltersDirective.controller('ClusterListFiltersCtrl', ['$scope', '$location', 'CurrentSearchState',
-    function($scope, $location, CurrentSearchState) {
+clusterListFiltersDirective.controller('ClusterListFiltersCtrl', ['$scope', '$routeParams', '$location',
+    function($scope, $routeParams, $location) {
 
-        function updateState() {
+        function updateState(view) {
             // Set location (URL)
-            $location.path(CurrentSearchState.getViewPath());
+            $location.path(view);
             $location.search({
-                q:CurrentSearchState.getQuery(),
-                page:CurrentSearchState.getPageNumber(),
-                size:CurrentSearchState.getPageSize()
+                q:$routeParams.q,
+                page:$routeParams.page,
+                size:$routeParams.size
             });
         }
 
-        // set filter sequence if init value provided
-        if ($scope.sequence) {
-            $scope.sequenceFilter = $scope.sequence;
-        }
-
-        $scope.totalResults = CurrentSearchState.getTotalResults();
-
         // attach filter-submit function
         $scope.listSubmit = function() {
-            // Keep the state change
-            CurrentSearchState.setViewPath("/");
-            updateState();
+            updateState("/list");
         }
 
         $scope.chartSubmit = function() {
-            // Keep the state change
-            CurrentSearchState.setViewPath("/chart");
-            updateState();
+            updateState("/chart");
         }
 
     }
