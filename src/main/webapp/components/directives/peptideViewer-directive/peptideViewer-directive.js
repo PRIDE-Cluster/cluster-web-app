680a50c47745d1a016519db028c412e9d8ad9981
Rui Wang <harry.r.wang@gmail.com>
2015-05-12 11:24:15 +0100

    added sorting to peptide viewer table

--- src/main/webapp/components/directives/peptideViewer-directive/peptideViewer-directive.js
+++ src/main/webapp/components/directives/peptideViewer-directive/peptideViewer-directive.js
@@ -6,64 +6,67 @@
  *
  */
 
-var peptideViewerDirective = angular.module('prideClusterApp.peptideViewerDirective', ['ngTable'])
+var peptideViewerDirective = angular.module('prideClusterApp.peptideViewerDirective', ['ngTable']);
 
-peptideViewerDirective.directive('prcPeptideViewer', ['ClusterPeptides', 'ngTableParams', function(ClusterPeptides, ngTableParams) {
-    function link(scope, element, attrs) {
+peptideViewerDirective.directive('prcPeptideViewer', function () {
+    return {
+        restrict: 'EA',
+        scope: {
+            clusterId: '=clusterid'
+        },
+        controller: 'PeptideViewerCtrl',
+        //link: link,
+        templateUrl: 'components/directives/peptideViewer-directive/peptideViewer-directive.html'
+    };
+});
+
+peptideViewerDirective.controller("PeptideViewerCtrl", ['$scope', '$filter', 'ClusterPeptides', 'ngTableParams', function($scope, $filter, ClusterPeptides, ngTableParams) {
+    $scope.peptides = [];
+
+    $scope.getPeptides = function() {
         ClusterPeptides.get(
-            { clusterId: scope.clusterId },
-            function(peptides) {
+            {clusterId: $scope.clusterId},
+            function (data) {
 
-                scope.peptides = peptides.clusteredPeptides;
-
-                //function compare(p1, p2) {
-                //    if (p1.numberOfPSMs < p2.numberOfPSMs) {
-                //        return 1;
-                //    }
-                //
-                //    if (p1.numberOfPSMs > p2.numberOfPSMs) {
-                //        return -1;
-                //    }
-                //
-                //    return 0;
-                //}
-                //
-                //scope.peptides.sort(compare);
+                $scope.peptides = data.clusteredPeptides;
 
                 // here we create a simplified modification list for each peptide that is used for
                 // showing tooltips as required by the <prc-peptide-sequence-viewer> directive
-                for (j = 0; j < scope.peptides.length; j++) {
-                    var peptide = scope.peptides[j];
+                for (j = 0; j < $scope.peptides.length; j++) {
+                    var peptide = $scope.peptides[j];
                     peptide.mods = [];
-                    if (peptide.modifications!=null) {
+                    if (peptide.modifications != null) {
                         for (i = 0; i < peptide.modifications.length; i++) {
                             peptide.mods[peptide.modifications[i].mainPosition - 1] = peptide.modifications[i].name;
                         }
                     }
                 }
 
-                scope.tableParams = new ngTableParams({
-                    page: 1,            // show first page
-                    count: 10,           // count per page
-                    sorting: {
-                        numberOfPSMs: 'asc'
-                    }
-                }, {
-                    total: scope.peptides.length, // length of data
-                    getData: function($defer, params) {
-                        scope.peptides_slice = scope.peptides.slice((params.page() - 1) * params.count(), params.page() * params.count());
-                        $defer.resolve(scope.peptides_slice);
-                    }
-                });
+                $scope.tableParams.reload();
             }
         );
-    }
-    return {
-        restrict: 'EA',
-        scope: {
-            clusterId: '=clusterid'
-        },
-        link: link,
-        templateUrl: 'components/directives/peptideViewer-directive/peptideViewer-directive.html'
+
+
     };
+
+    $scope.tableParams = new ngTableParams({
+            page: 1,            // show first page
+            count: 10,           // count per page
+            sorting: {
+                numberOfPSMs: 'desc'
+            }
+        }, {
+            total: $scope.peptides.length, // length of data
+            getData: function($defer, params) {
+
+                // use build-in angular filter
+                var orderedData = params.sorting() ?
+                    $filter('orderBy')($scope.peptides, params.orderBy()) :
+                    data;
+
+                params.total(orderedData.length);
+                $scope.peptides_slice = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+                $defer.resolve($scope.peptides_slice);
+            }
+    });
 }]);
